1e112a45268b6a5f22c39ba6cc86f8b660319ea2
Surja1997 <surjashish@gmail.com>
2020-02-07 14:56:26 +0530

    binary tree added, deletion pending

--- data structures/BinarySearchTree.py
+++ data structures/BinarySearchTree.py
@@ -20,7 +20,43 @@ def insert(root, node):
             else:
                 insert(root.left, node)
         else:
-            print("The value is alread present in the Binary search tree")
+            pass
 
-# def printInorder(root):
-#     if root.left is None
+
+def printInorder(root):
+    if root:
+        printInorder(root.left)
+        print(root.value)
+        printInorder(root.right)
+
+
+def same_height_nodes(root):
+    list_of_nodes, k, list_of_nodes_temp = [root], 1, []
+    if root:
+        print("Height wise printing starts: ")
+        print()
+    print(root.value)
+    while k == 1:
+        k = 0
+        for node in list_of_nodes:
+            if node.left:
+                print(node.left.value, end=" ")
+                list_of_nodes_temp.append(node.left)
+                k = 1
+            if node.right:
+                print(node.right.value, end=" ")
+                list_of_nodes_temp.append(node.right)
+                k = 1
+        print()
+        list_of_nodes=list_of_nodes_temp.copy()
+        list_of_nodes_temp.clear()
+
+
+a = [8,3,10,1,6,14,13,7,4]
+root_val = Node(a[0])
+for x in a:
+    insert(root_val, Node(x))
+
+printInorder(root_val)
+
+same_height_nodes(root_val)
