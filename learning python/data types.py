0e7ec8b6bb80654b2339ed1df727e6add35e91d4
Surja1997 <pialisarkar1968@gmail.com>
2020-12-25 06:57:26 +0530

    string introduction

---
+++ learning python/data types.py
@@ -0,0 +1,7 @@
+import random
+
+x = 3e10
+print(type(x))
+
+for i in range(0, 100):
+    print(random.randrange(1, 10, 2))
---
+++ learning python/strings.py
@@ -0,0 +1,12 @@
+#Strings are arrays
+a="Hello Surjashish"
+print(a[1])
+for x in a:
+    print(str(x.lower()))
+
+print("Surjashish" in a)
+
+a1=[45,33,22,656,24,32]
+print(45 in a1)
+print(4533 in a1)
+
